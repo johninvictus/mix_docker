2dd05fceb9807f462342139bd55f42180f65c031
johninvictus <invictusnitude@gmail.com>
2018-12-22 17:11:17 +0300

    updated distillery again

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule MixDocker.Mixfile do
 
   defp deps do
     [
-      {:distillery, "~> 2.0"},
+      {:distillery, "~> 2.0.0-rc.11"},
       {:ex_doc, "~> 0.10", only: :dev},
       {:plug_cowboy, "~> 2.0"}
     ]
